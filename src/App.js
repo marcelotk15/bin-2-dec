f9566c50e523cf638509ba4e2062ea608e464a93
Marcelo Augusto <marcelotk_15@hotmail.com>
2020-06-29 22:58:44 -0300

    created the app and working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Row,
+  H1,
+  Alert,
+  Fieldset,
+  Form,
+  Input,
+} from "@bootstrap-styled/v4";
 
 function App() {
+  const [binaryText, setBinaryText] = useState("");
+  const [decimalText, setDecimalText] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  //Binary input
+  useEffect(() => {
+    // Make sure we accept only either 0 or 1
+    if (binaryText.match(/^[0-1]+$/g) === null && binaryText !== "") {
+      setErrorMessage("Enter either 0 or 1");
+
+      return;
+    }
+
+    if (binaryText === "") return;
+
+    setErrorMessage("");
+
+    const reversedBinaryText = binaryText
+      .split("")
+      .map(Number) // Convert to a number from string
+      .reverse();
+
+    // Calculate the result by accumulating previous vaue
+    const result = reversedBinaryText.reduce(
+      (accumulator, currentValue, idx) =>
+        accumulator + currentValue * Math.pow(2, idx)
+    );
+    setDecimalText(result);
+  }, [binaryText]);
+
+  useEffect(() => {
+    setBinaryText((decimalText >>> 0).toString(2));
+    console.log(decimalText);
+  }, [decimalText]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Row>
+          <H1>Bin2Dec</H1>
+        </Row>
+
+        {errorMessage && <Alert color="danger">{errorMessage}</Alert>}
+
+        <Row>
+          <Form>
+            <Fieldset>
+              Binary:{" "}
+              <Input
+                type="number"
+                name="binary"
+                placeholder="Enter 0 or 1"
+                value={binaryText}
+                onChange={(e) => setBinaryText(e.target.value)}
+              />
+              Decimal:{" "}
+              <Input
+                type="number"
+                name="decimal"
+                value={decimalText}
+                onChange={(e) => setDecimalText(e.target.value)}
+              />
+            </Fieldset>
+          </Form>
+        </Row>
+      </Container>
+    </>
   );
 }
 
